4e7013abfb7d0987d4a213de685d15c92db688ec
Kyle Runge <krunge1@hotmail.com>
2023-03-12 09:55:20 -0500

    comments out

--- TestCafe.java
+++ TestCafe.java
@@ -11,31 +11,31 @@ public class TestCafe {
         
         /* ============ App Test Cases ============= */
     
-        // System.out.println("\n----- Streak Goal Test -----");
-        // System.out.printf("Purchases needed by week 10: %s \n\n", cafeUtilApp.getStreakGoal());
+        System.out.println("\n----- Streak Goal Test -----");
+        System.out.printf("Purchases needed by week 10: %s \n\n", cafeUtilApp.getStreakGoal());
     
-        // System.out.println("----- Order Total Test-----");
-        // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
-        // System.out.printf("Order total: $%.2f \n\n",cafeUtilApp.getOrderTotal(lineItems));
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: $%.2f \n\n",cafeUtilApp.getOrderTotal(lineItems));
         
-        // System.out.println("----- Display Menu Test-----");
+        System.out.println("----- Display Menu Test-----");
         
-        // ArrayList<String> menu = new ArrayList<String>();
-        // menu.add("drip coffee");
-        // menu.add("cappuccino");
-        // menu.add("latte");
-        // menu.add("mocha");
-        // cafeUtilApp.displayMenu(menu);
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        cafeUtilApp.displayMenu(menu);
     
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
-        // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     cafeUtilApp.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            cafeUtilApp.addCustomer(customers);
+            System.out.println("\n");
+        }
 
-        System.out.println("\n----- Price Chart Test-----");
-        cafeUtilApp.printPriceChart("Folders",10, 5);
+        // System.out.println("\n----- Price Chart Test-----");
+        // cafeUtilApp.printPriceChart("Folders",10, 5);
     }
 }
